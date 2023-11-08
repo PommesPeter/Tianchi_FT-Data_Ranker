43ed54d7baba9f84efbd9e7ad0ce878ca570b58b
JosonChan <JosonChan1998@163.com>
2023-11-08 20:35:20 +0800

    [Feature]: Add output_text_length_filter

--- data-juicer/data_juicer/ops/filter/__init__.py
+++ data-juicer/data_juicer/ops/filter/__init__.py
@@ -1,22 +1,11 @@
-from . import (
-    alphanumeric_filter,
-    average_line_length_filter,
-    character_repetition_filter,
-    flagged_words_filter,
-    language_id_score_filter,
-    maximum_line_length_filter,
-    perplexity_filter,
-    special_characters_filter,
-    specified_field_filter,
-    specified_numeric_field_filter,
-    stopwords_filter,
-    suffix_filter,
-    text_length_filter,
-    token_num_filter,
-    word_num_filter,
-    word_repetition_filter,
-    error_filter,
-)
+from . import (alphanumeric_filter, average_line_length_filter,
+               character_repetition_filter, error_filter, flagged_words_filter,
+               language_id_score_filter, maximum_line_length_filter,
+               output_text_length_filter, perplexity_filter,
+               special_characters_filter, specified_field_filter,
+               specified_numeric_field_filter, stopwords_filter, suffix_filter,
+               text_length_filter, token_num_filter, word_num_filter,
+               word_repetition_filter)
 
 __all__ = [
     'alphanumeric_filter',
@@ -37,4 +26,5 @@ __all__ = [
     'word_num_filter',
     'word_repetition_filter',
     'error_filter',
+    'output_text_length_filter',
 ]
