655612068726668fd732b6d7de4e4eb3ac2f39fc
PommesPeter <434596665@qq.com>
2023-11-10 10:31:07 +0800

    :sparkles: Feaature: added new ops

--- data-juicer/data_juicer/ops/filter/lm_score_evaluation_filter.py
+++ data-juicer/data_juicer/ops/filter/lm_score_evaluation_filter.py
@@ -1,6 +1,8 @@
 from jsonargparse.typing import ClosedUnitInterval
 from loguru import logger
 
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
 from data_juicer.utils.constant import Fields, StatsKeys
 from data_juicer.utils.model_utils import prepare_model, get_model
 
@@ -14,10 +16,10 @@ class LanguageModelEvaluationFilter(Filter):
 
     def __init__(
         self,
-        lang: str = '',
+        lang: str = 'en',
         min_score: ClosedUnitInterval = 0.8,
-        dimension: str = 'helpfulness',
-        model_name_or_path='internlm/internlm-chat-20b',
+        dimension: str = 'accuracy',
+        model_name_or_path='internlm/internlm-chat-7b',
         cuda_device: str = 'cuda:0',
         *args,
         **kwargs
@@ -39,11 +41,10 @@ class LanguageModelEvaluationFilter(Filter):
             "zh": "请检查模型是否加载成功。如果没有，请稍后重试。",
         }
 
-        from transformers import AutoTokenizer, AutoModelForCausalLM
-
         logger.info("Loading language model from HuggingFace...")
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
-        self.lm_model = AutoModelForCausalLM.from_pretrained(model_name_or_path).cuda(cuda_device).eval()
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, trust_remote_code=True)
+        self.lm_model = AutoModelForCausalLM.from_pretrained(model_name_or_path, trust_remote_code=True)
+        self.lm_model = self.lm_model.cuda(cuda_device).eval()
         
 
     def compute_stats(self, sample):
@@ -55,15 +56,14 @@ class LanguageModelEvaluationFilter(Filter):
         _instruction = sample['instruction'].lower().replace('\n', ' ')
         _input = sample['input'].lower().replace('\n', ' ')
         _output = sample['output'].lower().replace('\n', ' ')
-        # ft_model = get_model(self.model_key, lang=self.lang, model_type='fasttext')
-        # TODO: using prompt and giving to the language model to predict text quality
+
         if self.lm_model is None:
             err_msg = 'Model not loaded. Please retry later.'
             logger.error(err_msg)
             raise ValueError(err_msg)
         prompt = self.prompt[self.lang].format(_instruction, _input, _output)
-        pred = self.lm_model.chat(prompt, tokenizer=self.tokenizer)
-        language_model_score = 0
+        response, history = self.lm_model.chat(prompt, tokenizer=self.tokenizer)
+        language_model_score = int(response)
 
         sample[Fields.stats][StatsKeys.lm_eval_score] = language_model_score
 
