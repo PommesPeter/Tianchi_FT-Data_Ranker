2368d823e03c735dba78f047428cd9e5cbdb4ca1
JosonChan <JosonChan1998@163.com>
2023-11-15 21:58:46 +0800

    [Feature]: Add lang mapper

--- data-juicer/tests/ops/mapper/test_remove_specific_lang_mapper.py
+++ data-juicer/tests/ops/mapper/test_remove_specific_lang_mapper.py
@@ -7,20 +7,33 @@ from data_juicer.ops.mapper.remove_specific_lang_mapper import \
 class RemoveSpecificLanguagesMapperTest(unittest.TestCase):
 
     def setUp(self):
-        self.op = RemoveSpecificLanguagesMapper(
-            ['Korean', 'Chinese', 'Japanese'])
+        self.korean_op = RemoveSpecificLanguagesMapper(
+            langs_to_remove='Korean')
+        self.japanese_op = RemoveSpecificLanguagesMapper(
+            langs_to_remove='Japanese')
+        self.chinese_op = RemoveSpecificLanguagesMapper(
+            langs_to_remove='Chinese')
 
-    def _run_helper(self, samples):
+    def _run_korean_helper(self, samples):
         for sample in samples:
-            result = self.op.process(sample)
-            print(result['text'])
+            result = self.korean_op.process(sample)
             self.assertEqual(result['text'], result['target'])
 
-    def test_only_list_text(self):
+    def _run_chinese_helper(self, samples):
+        for sample in samples:
+            result = self.chinese_op.process(sample)
+            self.assertEqual(result['text'], result['target'])
+
+    def _run_japanese_helper(self, samples):
+        for sample in samples:
+            result = self.japanese_op.process(sample)
+            self.assertEqual(result['text'], result['target'])
+
+    def test_korean_text(self):
 
         samples = [{
-            'text': 'continue\n我是一个中国人지금 번역하기',
-            'target': 'continue\n '
+            'text': 'continue\n 지금 번역하기',
+            'target': 'continue\n Translate now'
         }, {
             'text':
             'great. Author, please indicate the final completed puzzle options \
@@ -33,10 +46,34 @@ class RemoveSpecificLanguagesMapperTest(unittest.TestCase):
                     in tabular form. Each row displays the question number, \
                     location, situation, and puzzle type. Please explain in \
                     detail.\n\nPlease write in formal tone, expository \
-                    writing style, English language.\n \n \n \n '
+                    writing style, English language.\n \n \n \n Translate now'
         }]
 
-        self._run_helper(samples)
+        self._run_korean_helper(samples)
+
+    def test_chinese_text(self):
+
+        samples = [{
+            'text': 'continue\n 我是一个中国人',
+            'target': 'continue\n I\'m Chinese.'
+        }]
+
+        self._run_chinese_helper(samples)
+
+    def test_japanese_text(self):
+
+        samples = [{
+            'text':
+            'continue\n この文章をCEFR B1相当の750～800単語の文章に書き換えてください',
+            'target':
+            'continue\n Please rewrite this text to 750 to 800 words equivalent\
+             to CEFR B1'
+        }, {
+            'text': 'continue\n',
+            'target': 'continue\n'
+        }]
+
+        self._run_japanese_helper(samples)
 
 
 if __name__ == '__main__':
